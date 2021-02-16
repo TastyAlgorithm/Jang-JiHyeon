494f46c0283f584f6cc60c67d73da39da01bf5e4
TastyAlgorithm <76274621+TastyAlgorithm@users.noreply.github.com>
2021-02-16 11:32:50 +0900

    Create B1967.java

---
+++ B1967.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class B1967 {
+
+    static int v,u, r;
+    static ArrayList<Node>[] adj;
+    static int[] dist;
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+       adj = (ArrayList<Node>[]) new ArrayList[10001];
+       dist = new int[10001];
+
+        for (int i = 0; i <=10000 ; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        
+        int n = sc.nextInt();
+        for (int i = 0; i <n-1 ; i++) {
+            int parent = sc.nextInt();
+            int child = sc.nextInt();
+            int weight = sc.nextInt();
+
+            adj[parent].add(new Node(child, weight));
+            adj[child].add(new Node(parent, weight));
+        }
+
+        dfs(1,0); 
+        r=0;
+        dist = new int[10001];
+        dfs(u,0);
+        System.out.println(r);
+    }
+
+    private static void dfs(int v, int d){
+        dist[v] = d;
+
+        if(dist[v] > r){
+            r = dist[v];
+            u = v;
+        }
+
+        for (Node node :  adj[v]){
+            int next = node.v;
+            int weight = node.w;
+
+            if(dist[next] == 0){
+                dfs(next, d + weight);
+            }
+        }
+    }
+    public static class Node{
+        int v;
+        int w;
+
+        public Node(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+    }
+
+}
