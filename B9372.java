a0eeab3c2c3690b0660863abecab7fcbbc2f96ac
TastyAlgorithm <76274621+TastyAlgorithm@users.noreply.github.com>
2021-02-16 11:33:07 +0900

    Create B9372.java

---
+++ B9372.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class B9372 {	
+	public static int N, M;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int i = 1 ; i <= T ; i++ ) {
+			N = sc.nextInt();
+			M = sc.nextInt();
+			for( int j = 0 ; j < M ; j++ ) {
+				int a = sc.nextInt()-1;
+				int b = sc.nextInt()-1;
+			}
+			System.out.println(N-1);
+		}
+	}
+}
